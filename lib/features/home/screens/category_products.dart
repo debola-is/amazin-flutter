27393916930bf377a4bee2a1f4c93b538d3993d4
debola-is <adebolasanni079@gmail.com>
2022-09-21 17:35:20 +0100

    UI changes

--- lib/features/home/screens/category_products.dart
+++ lib/features/home/screens/category_products.dart
@@ -1,4 +1,7 @@
 import 'package:amazin/constants/global_variables.dart';
+import 'package:amazin/features/admin/widgets/loader.dart';
+import 'package:amazin/features/home/services/home_services.dart';
+import 'package:amazin/models/product.dart';
 import 'package:flutter/material.dart';
 
 class CategoryProducts extends StatefulWidget {
@@ -14,6 +17,21 @@ class CategoryProducts extends StatefulWidget {
 }
 
 class _CategoryProductsState extends State<CategoryProducts> {
+  List<Product>? productsList;
+  final HomeServices homeServices = HomeServices();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getCategoryProducts();
+  }
+
+  Future<void> getCategoryProducts() async {
+    productsList = await homeServices.getProductForCategory(
+        context: context, category: widget.category);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,39 +53,187 @@ class _CategoryProductsState extends State<CategoryProducts> {
               ),
             )),
       ),
-      body: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 15,
-              vertical: 10,
-            ),
-            alignment: Alignment.topLeft,
-            child: Text(
-              'Keep shopping for ${widget.category}',
-              style: const TextStyle(
-                color: Colors.black,
-                fontSize: 20,
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 170,
-            child: GridView.builder(
-              itemCount: 10,
-              scrollDirection: Axis.horizontal,
-              padding: const EdgeInsets.only(left: 15),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 1,
-                childAspectRatio: 1.4,
-                mainAxisSpacing: 10,
-              ),
-              itemBuilder: (context, index) {
-                return Text("Hello");
-              },
-            ),
-          )
-        ],
+      body: SingleChildScrollView(
+        child: productsList == null
+            ? const Loader()
+            : productsList!.isEmpty
+                ? const Center(
+                    child: Text(
+                      'No Product in this category for now',
+                    ),
+                  )
+                : Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
+                        ),
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'Keep shopping for ${widget.category}',
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 200,
+                        child: GridView.builder(
+                          itemCount: productsList!.length,
+                          scrollDirection: Axis.horizontal,
+                          padding: const EdgeInsets.only(left: 15),
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 1,
+                            childAspectRatio: 1.4,
+                            mainAxisSpacing: 10,
+                          ),
+                          itemBuilder: (context, index) {
+                            final product = productsList![index];
+                            return Column(
+                              children: [
+                                SizedBox(
+                                  height: 130,
+                                  width: 130,
+                                  child: DecoratedBox(
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: Colors.black12,
+                                        width: 0.5,
+                                      ),
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(10),
+                                      child: Image.network(
+                                        product.images[0],
+                                        loadingBuilder:
+                                            (context, child, loadingProgress) {
+                                          if (loadingProgress == null) {
+                                            return child;
+                                          }
+                                          return Center(
+                                            child: CircularProgressIndicator(
+                                              value: loadingProgress
+                                                          .expectedTotalBytes !=
+                                                      null
+                                                  ? loadingProgress
+                                                          .cumulativeBytesLoaded /
+                                                      loadingProgress
+                                                          .expectedTotalBytes!
+                                                  : null,
+                                            ),
+                                          );
+                                        },
+                                        frameBuilder: (context, child, frame,
+                                            wasSynchronouslyLoaded) {
+                                          return child;
+                                        },
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      product.name,
+                                      style: TextStyle(
+                                        color:
+                                            GlobalVariables.selectedNavBarColor,
+                                        fontSize: 12,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 1,
+                                    ),
+                                    Text(
+                                      product.description,
+                                      style: const TextStyle(
+                                        color: Colors.black45,
+                                        fontSize: 10,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 2,
+                                    ),
+                                  ],
+                                )
+                              ],
+                            );
+                          },
+                        ),
+                      ),
+                      Text(
+                        'Demo Headline 2',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Card(
+                        child: ListTile(
+                            title: Text('Motivation $int'),
+                            subtitle: Text(
+                                'this is a description of the motivation')),
+                      ),
+                      Card(
+                        child: ListTile(
+                            title: Text('Motivation $int'),
+                            subtitle: Text(
+                                'this is a description of the motivation')),
+                      ),
+                      Card(
+                        child: ListTile(
+                            title: Text('Motivation $int'),
+                            subtitle: Text(
+                                'this is a description of the motivation')),
+                      ),
+                      Card(
+                        child: ListTile(
+                            title: Text('Motivation $int'),
+                            subtitle: Text(
+                                'this is a description of the motivation')),
+                      ),
+                      Card(
+                        child: ListTile(
+                            title: Text('Motivation $int'),
+                            subtitle: Text(
+                                'this is a description of the motivation')),
+                      ),
+                      Text(
+                        'Demo Headline 2',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Card(
+                        child: ListTile(
+                            title: Text('Motivation $int'),
+                            subtitle: Text(
+                                'this is a description of the motivation')),
+                      ),
+                      Card(
+                        child: ListTile(
+                            title: Text('Motivation $int'),
+                            subtitle: Text(
+                                'this is a description of the motivation')),
+                      ),
+                      Card(
+                        child: ListTile(
+                            title: Text('Motivation $int'),
+                            subtitle: Text(
+                                'this is a description of the motivation')),
+                      ),
+                      Card(
+                        child: ListTile(
+                            title: Text('Motivation $int'),
+                            subtitle: Text(
+                                'this is a description of the motivation')),
+                      ),
+                      Card(
+                        child: ListTile(
+                            title: Text('Motivation $int'),
+                            subtitle: Text(
+                                'this is a description of the motivation')),
+                      ),
+                    ],
+                  ),
       ),
     );
   }
