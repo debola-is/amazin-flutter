e2ee328971d598317c35794f53bab69eb493df77
debola-is <adebolasanni079@gmail.com>
2022-09-21 17:32:52 +0100

    removed auth token header from getCategoryProducts since unregistered user should be able to see products

--- lib/features/home/services/home_services.dart
+++ lib/features/home/services/home_services.dart
@@ -4,22 +4,18 @@ import 'package:amazin/constants/error_handling.dart';
 import 'package:amazin/constants/global_variables.dart';
 import 'package:amazin/constants/utils.dart';
 import 'package:amazin/models/product.dart';
-import 'package:amazin/providers/user_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
-import 'package:provider/provider.dart';
 
 class HomeServices {
   Future<List<Product>> getProductForCategory(
       {required BuildContext context, required String category}) async {
-    final userProvider = Provider.of<UserProvider>(context, listen: false);
     List<Product> allProducts = [];
     try {
       http.Response response = await http.get(
         Uri.parse('$uri/api/products/$category'),
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
-          'user-auth-token': userProvider.user.token,
         },
       );
 
