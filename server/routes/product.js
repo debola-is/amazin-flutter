446c5d171009575e3187b170ccd44be741badb96
debola-is <adebolasanni079@gmail.com>
2022-09-22 18:36:36 +0100

    added product search api

--- server/routes/product.js
+++ server/routes/product.js
@@ -11,8 +11,26 @@ productRouter.get("/api/products/:category", async (req, res)=>{
         return res.status(400).json({error: "Bad request parameters!"});
     }
     try{
-        const categoryPproducts = await Product.find({category});
-        return res.json(categoryPproducts);
+        const categoryProducts = await Product.find({category});
+        return res.json(categoryProducts);
+    }
+    catch(e){
+        return res.status(500).json({error: e.message});
+    }
+
+});
+
+
+productRouter.get("/api/products/search/:searchQuery", async (req, res)=>{
+    
+    const query= req.params.searchQuery;
+    
+    try{
+        const products = await Product.find({
+            name: {$regex: query, 
+            $options: "i"},
+        });
+        return res.json(products);
     }
     catch(e){
         return res.status(500).json({error: e.message});
