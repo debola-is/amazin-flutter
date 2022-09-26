16af701d78eb88faaab933dff5c353d061699a90
Debola <adebolasanni079@gmail.com>
2022-09-26 06:39:03 +0100

    Now accepts error type parameter to distinguish different snackBars based on error type and changes look of snackBar

--- lib/constants/utils.dart
+++ lib/constants/utils.dart
@@ -1,14 +1,54 @@
 import 'dart:io';
-
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
-void showSnackBar(BuildContext context, String text) {
+void showSnackBar(
+  BuildContext context,
+  String text,
+  String type,
+) {
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
-      content: Text(
-        text,
+      padding: const EdgeInsets.all(15),
+      content: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Text(
+              text,
+              style: TextStyle(
+                fontStyle: FontStyle.italic,
+                color: type == "error"
+                    ? Colors.red.shade900
+                    : type == "success"
+                        ? Colors.green.shade900
+                        : Colors.black,
+                overflow: TextOverflow.ellipsis,
+              ),
+              maxLines: 5,
+            ),
+          ),
+          Icon(
+            type == "error"
+                ? Icons.error_outline_outlined
+                : type == "success"
+                    ? Icons.check_outlined
+                    : Icons.info_outline,
+            color: type == "error"
+                ? const Color.fromARGB(255, 168, 0, 0)
+                : type == "success"
+                    ? const Color.fromARGB(255, 18, 95, 23)
+                    : Colors.black,
+            size: 18,
+          )
+        ],
       ),
+      backgroundColor: type == "error"
+          ? const Color.fromARGB(255, 249, 225, 227)
+          : type == "success"
+              ? const Color.fromARGB(255, 236, 249, 236)
+              : const Color.fromARGB(255, 250, 248, 229),
+      behavior: SnackBarBehavior.floating,
     ),
   );
 }
