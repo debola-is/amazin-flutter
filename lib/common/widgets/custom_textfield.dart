943beb3ac2254332281f7d7736f7862e2091a6e5
Debola <adebolasanni079@gmail.com>
2022-09-26 06:37:26 +0100

    added obscureText property for password inputs

--- lib/common/widgets/custom_textfield.dart
+++ lib/common/widgets/custom_textfield.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
   final int maxLines;
-  final TextInputType inputType;
+  final TextInputType? inputType;
+  final bool obscureText;
+
   const CustomTextField({
     Key? key,
     required this.controller,
     required this.hintText,
     this.maxLines = 1,
-    this.inputType = TextInputType.text,
+    this.obscureText = false,
+    this.inputType,
   }) : super(key: key);
 
   @override
@@ -21,6 +23,7 @@ class CustomTextField extends StatelessWidget {
     return TextFormField(
       controller: controller,
       keyboardType: inputType,
+      obscureText: obscureText,
       decoration: InputDecoration(
         hintText: hintText,
         border: const OutlineInputBorder(
